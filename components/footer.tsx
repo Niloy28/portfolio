2d7811128e1c891a55b4fb22e906f36ba3597ce9
Farhan Ishrak Islam <niloy28@live.com>
2025-03-15 22:08:48 +0900

    Make footer responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,9 +5,12 @@ import { SocialIcon } from "react-social-icons";
 const Footer = () => {
 	return (
 		<footer className="flex flex-col gap-4 justify-center items-center p-8 pb-12">
-			<div className="flex gap-2 font-bold text-foreground/50">
-				Copyright <Copyright /> {new Date().getFullYear()} Farhan Ishrak Islam.
-				All Rights Reserved.
+			<div className="flex flex-col sm:flex-row justify-center items-center gap-2 font-bold text-foreground/50">
+				<p className="flex gap-2">
+					Copyright <Copyright /> {new Date().getFullYear()} Farhan Ishrak
+					Islam.
+				</p>
+				<p>All Rights Reserved.</p>
 			</div>
 			<div className="flex items-center gap-2">
 				<SocialIcon url="https://www.linkedin.com/in/farhan-ishrak-islam-26a7111b1/" />
